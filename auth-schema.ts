376a32df2c504a90a4ed8a2e385d7a0d36f600f9
utkarsh-1235 <utkarsh.saxena13sept@gmail.com>
2025-06-12 23:39:46 +0530

    Adding BetterAuth for authenticating and modifying schema

--- auth-schema.ts
+++ auth-schema.ts
@@ -1,29 +1,29 @@
-import {pgTable,timestamp, text, boolean } from "drizzle-orm/pg-core";
+import { pgTable, text, timestamp, boolean} from "drizzle-orm/pg-core";
 
 export const user = pgTable("user", {
-id: text('id').primaryKey(),
-name: text('name').notNull(),
+					id: text('id').primaryKey(),
+					name: text('name').notNull(),
  email: text('email').notNull().unique(),
  emailVerified: boolean('email_verified').$defaultFn(() => false).notNull(),
  image: text('image'),
  createdAt: timestamp('created_at').$defaultFn(() => /* @__PURE__ */ new Date()).notNull(),
  updatedAt: timestamp('updated_at').$defaultFn(() => /* @__PURE__ */ new Date()).notNull()
-                });
+				});
 
 export const session = pgTable("session", {
-                    id: text('id').primaryKey(),
-                    expiresAt: timestamp('expires_at').notNull(),
+					id: text('id').primaryKey(),
+					expiresAt: timestamp('expires_at').notNull(),
  token: text('token').notNull().unique(),
  createdAt: timestamp('created_at').notNull(),
  updatedAt: timestamp('updated_at').notNull(),
  ipAddress: text('ip_address'),
  userAgent: text('user_agent'),
  userId: text('user_id').notNull().references(()=> user.id, { onDelete: 'cascade' })
-                });
+				});
 
 export const account = pgTable("account", {
-id: text('id').primaryKey(),
-accountId: text('account_id').notNull(),
+					id: text('id').primaryKey(),
+					accountId: text('account_id').notNull(),
  providerId: text('provider_id').notNull(),
  userId: text('user_id').notNull().references(()=> user.id, { onDelete: 'cascade' }),
  accessToken: text('access_token'),
@@ -35,11 +35,11 @@ accountId: text('account_id').notNull(),
  password: text('password'),
  createdAt: timestamp('created_at').notNull(),
  updatedAt: timestamp('updated_at').notNull()
-                });
+				});
 
 export const verification = pgTable("verification", {
-id: text('id').primaryKey(),
-identifier: text('identifier').notNull(),
+					id: text('id').primaryKey(),
+					identifier: text('identifier').notNull(),
  value: text('value').notNull(),
  expiresAt: timestamp('expires_at').notNull(),
  createdAt: timestamp('created_at').$defaultFn(() => /* @__PURE__ */ new Date()),
