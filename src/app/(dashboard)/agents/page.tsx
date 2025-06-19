1615d584750fa2f11f80ce447e47a75d28ee929b
utkarsh-1235 <utkarsh.saxena13sept@gmail.com>
2025-06-19 23:00:36 +0530

    Adding crreate agent component

--- src/app/(dashboard)/agents/page.tsx
+++ src/app/(dashboard)/agents/page.tsx
@@ -1,13 +1,26 @@
+import { auth } from "@/lib/auth";
+import { AgentsListheader } from "@/modules/agents/ui/components/agents-list-header";
 import { AgentsView, AgentsViewError, AgentsViewLoading } from "@/modules/agents/ui/views/agent-view";
 import { getQueryClient, trpc } from "@/trpc/server";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import { ErrorBoundary } from "next/dist/client/components/error-boundary";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 import { Suspense } from "react";
-const Page = () => {
+const Page = async() => {
+  const session = await auth.api.getSession({
+        headers: await headers()
+    })
+  
+    if(!session){
+      redirect("/sign-in")
+    }
     const queryClient = getQueryClient();
     void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions());
 
  return (
+  <>
+  <AgentsListheader/>
     <HydrationBoundary state={dehydrate(queryClient)}>
         <Suspense fallback={<AgentsViewLoading/>}>
  <ErrorBoundary errorComponent={AgentsViewError}>
@@ -15,6 +28,7 @@ const Page = () => {
  </ErrorBoundary>
    </Suspense>
   </HydrationBoundary>
+  </>
  )
 }
 
--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -1,12 +1,26 @@
 import { db } from "@/db";
 import { agents } from "@/db/schema";
-import {createTRPCRouter, baseProcedure, protectedProcedure} from "@/trpc/init";
+import {createTRPCRouter, protectedProcedure} from "@/trpc/init";
 import { agenstsInsertSchema } from "../schemas";
+import { z } from "zod";
+import { eq } from "drizzle-orm"; // Add this import
 // import { TRPCError } from "@trpc/server";
 
 
 export const agentRouter = createTRPCRouter({
-    getMany: baseProcedure.query(async()=>{
+    getOne: protectedProcedure.input(z.object({id: z.string()})).query(async({ input })=>{
+        const [existingAgent] = await db
+        .select()
+        .from(agents)
+        .where(eq(agents.id, input.id))
+    
+ 
+    // await new Promise((resolve) => setTimeout(resolve, 5000));
+    // throw new TRPCError({code: "BAD_REQUEST"});
+        
+        return existingAgent;
+    }),
+    getMany: protectedProcedure.query(async()=>{
         const data = await db
         .select()
         .from(agents)
