69e17d9d66f6a47ecc9e78477c491f0185994616
utkarsh-1235 <utkarsh.saxena13sept@gmail.com>
2025-06-20 22:34:47 +0530

    Adding empty component when there is no agent created

---
+++ src/components/ui/empty-state.tsx
@@ -0,0 +1,24 @@
+import { AlertCircleIcon} from "lucide-react";
+import Image from "next/image";
+
+interface Props {
+    title: string;
+    description: string;
+}
+
+export const EmptyState = ({
+    title,
+    description
+}: Props) => {
+    return (
+        <div className="flex flex-col items-center justify-center">
+               <Image src="/empty.svg" alt="Empty" width={240} height={240} className="text-muted-foreground"/>
+                <AlertCircleIcon className="size-6 text-red-500"/>
+                <div className="flex flex-col gap-y-6 max-w-medium mx-auto text-center">
+                    <h6 className="text-lg font-medium">{title}</h6>
+                    <p className="text-sm">{description}</p>
+                </div>
+               
+        </div>
+    )
+}
--- src/modules/agents/ui/views/agent-view.tsx
+++ src/modules/agents/ui/views/agent-view.tsx
@@ -5,6 +5,7 @@ import { LoadingState } from "@/components/ui/loading-state";
 import { ErrorState } from "@/components/ui/error-state";
 import { DataTable } from "../components/data-table";
 import { columns} from "../components/columns";
+import { EmptyState } from "@/components/ui/empty-state";
 
 export const AgentsView = () => {
     const trpc = useTRPC();
@@ -15,6 +16,11 @@ export const AgentsView = () => {
         <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4">
                 {/* {JSON.stringify(data, null, 2)} */}
                 <DataTable data={data} columns={columns}/>
+                {data.length === 0 && (
+                    <EmptyState
+                     title="Create your first agent"
+                     description="Create an agent to join your meetings"/>
+                )}
         </div>
     )
 }
