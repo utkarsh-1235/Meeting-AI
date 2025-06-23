23076a1163d5df27b15d5caafc6b53dc659f3f87
utkarsh-1235 <utkarsh.saxena13sept@gmail.com>
2025-06-23 23:40:50 +0530

    Modifying getMany procedure

--- src/components/ui/command-select.tsx
+++ src/components/ui/command-select.tsx
@@ -37,6 +37,11 @@ export const CommandSelect = ({
     const [open, setOpen] = useState(false);
     const selectedOption = options.find(option => option.value === value);
 
+    const handleOpenChange = (open: boolean) => {
+           onSearch?.("");
+           setOpen(open);
+    }
+
     return (
         <>
         <Button
@@ -54,8 +59,9 @@ export const CommandSelect = ({
            <ChevronsUpDownIcon/>
         </Button>
         <CommandResponsiveDialog
+        shouldFilter={!onSearch}
         open={open}
-        onOpenChange={setOpen}>
+        onOpenChange={handleOpenChange}>
                 <CommandInput placeholder="Search..." onValueChange={onSearch}/>
                 <CommandList>
                     <CommandEmpty>
